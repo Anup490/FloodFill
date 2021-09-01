780abfa9f3336f3f55979e6dfb37e7ffe13a52f4
Anup490 <anupbhandari490@gmail.com>
2021-09-01 20:51:53 +0530

    Added custom gamemodebase and ui screen

---
+++ Source/FloodFill/FloodFillWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "FloodFillWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOODFILL_API UFloodFillWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
